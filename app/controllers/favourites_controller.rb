94fd1cd296028a5c1aee37b1292d9bf52770768b
sofiiaabdu <sofiiaa@tahometer.com>
2018-09-18 13:53:48 +0300

    Refactor code.

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -3,65 +3,20 @@ require 'json'
 require 'uri'
 
 class FavouritesController < ApplicationController
+  before_action :set_url
 
   def index
-    def request_to_api(url)
-      api_key = '9a61a57a-9441-4c04-9bea-6da7ce1440b5'
-
-      uri = URI.parse(url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      header = {'Content-Type': 'application/json', 'x-api-key': api_key}
-      req = Net::HTTP::Get.new(uri, header)
-      response = http.request(req)
-      JSON.parse(response.read_body)
-    end
-
-    url_fav = 'https://api.thecatapi.com/v1/favourites/'
-    url_search = 'https://api.thecatapi.com/v1/images/'
-    @resp = request_to_api(url_fav)
-
-    # @resp.map! { |e| e['image_id'] }
+    @response = get_request(@url)
   end
 
   def create
-
-    api_key = '9a61a57a-9441-4c04-9bea-6da7ce1440b5'
-
-    url = URI.parse("https://api.thecatapi.com/v1/favourites")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Post.new(url)
-    request["Content-Type"] = 'application/json'
-    request["x-api-key"] = api_key
-
-    user = {'image_id': params['image_id'],
-            'sub_id': 'aqsoa7'
-    }
-
-    request.body = user.to_json
-    response = http.request(request)
-    response.read_body
-
+    post_request(@url)
     redirect_to "/favourites"
   end
 
-  def delete
+  private
 
-    api_key = '9a61a57a-9441-4c04-9bea-6da7ce1440b5'
-    url = URI.parse("https://api.thecatapi.com/v1/favourites")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Delete.new(url)
-    request["Content-Type"] = 'application/json'
-    request["x-api-key"] = api_key
-
-    response = http.request(request)
-    puts response.read_body
+  def set_url
+    @url = 'https://api.thecatapi.com/v1/favourites/'
   end
-
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,25 +4,7 @@ require 'json'
 class SearchController < ApplicationController
 
   def index
-
-    api_key = '9a61a57a-9441-4c04-9bea-6da7ce1440b5'
-
-    url = "https://api.thecatapi.com"
-    uri = URI.parse(url + "/v1/images/search")
-    response = Net::HTTP.get_response(uri)
-    response['Content-Type'] = 'application/json'
-    response['x-api-key'] = api_key
-    @rnd_img = JSON.parse(response.body)
-
-    respond_to do |format|
-      format.html do
-        render
-      end
-      format.json do
-        render json: {
-            url: "https://78.media.tumblr.com/tumblr_lzhyni85ln1qzv52ko1_500.jpg"
-        }
-      end
-    end
+    url = "https://api.thecatapi.com/v1/images/search"
+    @random_image = get_response(url)
   end
 end
